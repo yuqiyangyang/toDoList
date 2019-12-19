a2bb0069512b2660d9b4f99171bee04260b7ec99
yuqi <yuqi.yang@mail.mcgill.ca>
2019-12-18 20:48:02 -0500

    add AddToDo file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Todos from './Todos'
+import AddToDo from './AddToDo'
 
 
 
@@ -18,11 +19,19 @@ class App extends Component {
       todos
     })
   }
+  addTodo = (todo) =>{
+    todo.id= Math.random()
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+  }
   render() {
     return(
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's </h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddToDo addTodo={this.addTodo} />
 
       </div>
     );
